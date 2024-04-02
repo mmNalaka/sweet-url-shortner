e0e102cd59619fb9f9f922d85570a0171303bd0f
mmNalaka <nalaka.manathunga@gmail.com>
2024-04-02 21:14:11 +0200

    Initial commit

---
+++ cmd/server/wire/wire.go
@@ -0,0 +1,62 @@
+//go:build wireinject
+// +build wireinject
+
+package wire
+
+import (
+	"sweet/internal/handler"
+	"sweet/internal/repository"
+	"sweet/internal/server"
+	"sweet/internal/service"
+	"sweet/pkg/app"
+	"sweet/pkg/jwt"
+	"sweet/pkg/log"
+	"sweet/pkg/server/http"
+	"sweet/pkg/sid"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var repositorySet = wire.NewSet(
+	repository.NewDB,
+	//repository.NewRedis,
+	repository.NewRepository,
+	repository.NewTransaction,
+	repository.NewUserRepository,
+)
+
+var serviceSet = wire.NewSet(
+	service.NewService,
+	service.NewUserService,
+)
+
+var handlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
+
+var serverSet = wire.NewSet(
+	server.NewHTTPServer,
+	server.NewJob,
+)
+
+// build App
+func newApp(httpServer *http.Server, job *server.Job) *app.App {
+	return app.NewApp(
+		app.WithServer(httpServer, job),
+		app.WithName("demo-server"),
+	)
+}
+
+func NewWire(*viper.Viper, *log.Logger) (*app.App, func(), error) {
+
+	panic(wire.Build(
+		repositorySet,
+		serviceSet,
+		handlerSet,
+		serverSet,
+		sid.NewSid,
+		jwt.NewJwt,
+		newApp,
+	))
+}
